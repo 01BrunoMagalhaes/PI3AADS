998a6ff4128cf6ee93f8598706bcd835cf168862
Bruno Magalhães <bruno.magalhaes.parreira@gmail.com>
2018-05-16 20:36:13 -0300

    Teste funcional com json sendo exibindo em uma página.

--- Principal/Conexao.ino
+++ Principal/Conexao.ino
@@ -1,9 +1,10 @@
+#include <ArduinoJson.h>
 #include <SPI.h>
 #include <Ethernet.h>
 
 //Definicoes de IP, mascara de rede e gateway
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(192,168,15,2);          //Define o endereco IP
+IPAddress ip(192,168,15,4);          //Define o endereco IP
 IPAddress gateway(192,168,15,1);      //Define o gateway
 IPAddress subnet(255, 255, 255, 0); //Define a máscara de rede
 
@@ -17,8 +18,7 @@ void teste() {
   //Aguarda conexao do browser
   EthernetClient client = server.available();
   if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
+    Serial.println("Novo cliente");
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
@@ -28,43 +28,59 @@ void teste() {
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
         if (c == 'n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");
-          client.println("Refresh: 2"); //Recarrega a pagina a cada 2seg
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          //Configura o texto e imprime o titulo no browser
-          client.print("<font color=#FF0000><b><u>");
-          client.print("Envio de informacoes pela rede utilizando Arduino");
-          client.print("</u></b></font>");
-          client.println("<br />");
-          client.println("<br />");
-          //Mostra o estado da porta digital 3
-          int porta_digital = digitalRead(3);
-          client.print("Porta Digital 3 : ");
-          client.print("<b>");
-          client.print(porta_digital);
-          client.println("</b>");
-          client.print("  (0 = Desligada, 1 = Ligada)");
-          client.println("<br />");
-          break;
+          //montar aqui o json de acordo
+          // Allocate JsonBuffer
+  // Use arduinojson.org/assistant to compute the capacity.
+  StaticJsonBuffer<500> jsonBuffer;
+
+  // Create the root object
+  JsonObject& root = jsonBuffer.createObject();
+
+  // Create the "analog" array
+  JsonArray& analogValues = root.createNestedArray("analog");
+  for (int pin = 0; pin < 6; pin++) {
+    // Read the analog input
+    int value = analogRead(pin);
+
+    // Add the value at the end of the array
+    analogValues.add(value);
+  }
+
+  // Create the "digital" array
+  JsonArray& digitalValues = root.createNestedArray("digital");
+  for (int pin = 0; pin < 14; pin++) {
+    // Read the digital input
+    int value = digitalRead(pin);
+
+    // Add the value at the end of the array
+    digitalValues.add(value);
+  }
+
+  Serial.print(F("Sending: "));
+  root.printTo(Serial);
+  Serial.println();
+
+  // Write response headers
+  client.println("HTTP/1.0 200 OK");
+  client.println("Content-Type: application/json");
+  client.println("Connection: close");
+  client.println();
+
+  // Write JSON document
+  root.prettyPrintTo(client);
+
         }
         if (c == 'n') {
-          // you're starting a new line
           currentLineIsBlank = true;
-        } 
-        else if (c != 'r') {
-          // you've gotten a character on the current line
+        } else if (c != 'r') {
           currentLineIsBlank = false;
         }
+        
       }
     }
-    // give the web browser time to receive the data
+    // tempo para o browser receber os dados
     delay(1);
-    // close the connection:
+    // fecha a conexão
     client.stop();
     }
 }
