30bc293864f7501cc3e69b916ad7ba9a30d24811
Bruno Magalhães <bruno.magalhaes.parreira@gmail.com>
2018-07-03 11:18:48 -0300

    Incluindo horarios e potencias no JSON.

--- Principal/Conexao.ino
+++ Principal/Conexao.ino
@@ -44,7 +44,7 @@ void verificaClients() {
           
           if(!acao.equalsIgnoreCase("")){
             if(acao.equalsIgnoreCase("00 ")){
-              StaticJsonBuffer<500> jsonBuffer; JsonObject& root = jsonBuffer.createObject(); 
+              StaticJsonBuffer<1500> jsonBuffer; JsonObject& root = jsonBuffer.createObject(); 
               JsonArray& aquarioArray = root.createNestedArray("aquario");
               JsonObject& aquarioObject = jsonBuffer.createObject();
               aquarioArray.add(aquarioObject);
@@ -80,6 +80,62 @@ void verificaClients() {
               dataObject["data"] = retornarData();
               dataObject["hora"] = retornarHora();
 
+              JsonArray& potenciaAzulArray = aquarioObject.createNestedArray("potenciaAzul");
+              JsonObject& potenciaAzulObject = jsonBuffer.createObject();
+              potenciaAzulArray.add(potenciaAzulObject);
+              potenciaAzulObject["p0"] = b10;
+              potenciaAzulObject["p1"] = b11;
+              potenciaAzulObject["p2"] = b12;
+              potenciaAzulObject["p3"] = b13;
+              potenciaAzulObject["p4"] = b14;
+              potenciaAzulObject["p5"] = b15;
+              potenciaAzulObject["p6"] = b16;
+              potenciaAzulObject["p7"] = b17;
+              potenciaAzulObject["p8"] = b18;
+              potenciaAzulObject["p9"] = b19;
+
+              JsonArray& horaAzulArray = aquarioObject.createNestedArray("horaAzul");
+              JsonObject& horaAzulObject = jsonBuffer.createObject();
+              horaAzulArray.add(horaAzulObject);
+              horaAzulObject["h0"] = c10;
+              horaAzulObject["h1"] = c11;
+              horaAzulObject["h2"] = c12;
+              horaAzulObject["h3"] = c13;
+              horaAzulObject["h4"] = c14;
+              horaAzulObject["h5"] = c15;
+              horaAzulObject["h6"] = c16;
+              horaAzulObject["h7"] = c17;
+              horaAzulObject["h8"] = c18;
+              horaAzulObject["h9"] = c19;
+              
+              JsonArray& potenciaBrancoArray = aquarioObject.createNestedArray("potenciaBranco");
+              JsonObject& potenciaBrancoObject = jsonBuffer.createObject();
+              potenciaBrancoArray.add(potenciaBrancoObject);
+              potenciaBrancoObject["p0"] = b20;
+              potenciaBrancoObject["p1"] = b21;
+              potenciaBrancoObject["p2"] = b22;
+              potenciaBrancoObject["p3"] = b23;
+              potenciaBrancoObject["p4"] = b24;
+              potenciaBrancoObject["p5"] = b25;
+              potenciaBrancoObject["p6"] = b26;
+              potenciaBrancoObject["p7"] = b27;
+              potenciaBrancoObject["p8"] = b28;
+              potenciaBrancoObject["p9"] = b29;
+              
+              JsonArray& horaBrancoArray = aquarioObject.createNestedArray("horaBranco");
+              JsonObject& horaBrancoObject = jsonBuffer.createObject();
+              horaBrancoArray.add(horaBrancoObject);
+              horaBrancoObject["h0"] = c20;
+              horaBrancoObject["h1"] = c21;
+              horaBrancoObject["h2"] = c22;
+              horaBrancoObject["h3"] = c23;
+              horaBrancoObject["h4"] = c24;
+              horaBrancoObject["h5"] = c25;
+              horaBrancoObject["h6"] = c26;
+              horaBrancoObject["h7"] = c27;
+              horaBrancoObject["h8"] = c28;
+              horaBrancoObject["h9"] = c29;
+              
               client.println("HTTP/1.0 200 OK"); client.println("Content-Type: application/json"); client.println("Connection: close"); client.println("charset: utf-8");
               //client.println("Refresh: 5"); 
               client.println(); root.prettyPrintTo(client);  
